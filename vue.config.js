20ec404d4a0dc95f0c7be3712cc09756e25eb5d7
Dennis Huang <chop.chop.dennis@gmail.com>
2021-10-14 15:11:49 +0800

    Fix some config problem: add .env and tailwindcss.

--- vue.config.js
+++ vue.config.js
@@ -4,30 +4,20 @@ const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 module.exports = {
   transpileDependencies: ['vuetify'],
-  // devServer: {
-  //   port: 8082,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://strapi.machi.codes',
-  //       changeOrigin: true,
-  //       secure: true,
-  //       ws: true,
-  //       pathRewrite: { '^/api': '' },
-  //       logLevel: 'debug'
-  //     }
-  //   }
-  // }
-  // configureWebpack: (config) => {
-  //   return {
-  //     plugins: [
-  //       new PrerenderSPAPlugin({
-  //         staticDir: path.join(__dirname, 'dist'),
-  //         routes: ['/', '/about'],
-  //         renderer: new Renderer({
-  //           renderAfterDocumentEvent: 'render-event'
-  //         })
-  //       })
-  //     ]
-  //   }
-  // }
+  devServer: {
+    disableHostCheck: true,
+  },
+  configureWebpack: (config) => {
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'dist'),
+          routes: ['/'],
+          renderer: new Renderer({
+            renderAfterDocumentEvent: 'render-event'
+          })
+        })
+      ]
+    }
+  }
 }
